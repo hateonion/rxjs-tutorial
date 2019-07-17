f828a494a7fe92af266b79f8be625fa949880be7
hateonion <ouyang_yingjie@outlook.com>
2019-07-17 21:48:26 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { AsyncSubject } from 'rxjs'; 
-import { share } from 'rxjs/operators';
+import { Observable, interval } from 'rxjs'; 
+import { map } from 'rxjs/operators';
 
-const subject = new AsyncSubject();
+const observable = interval(1000).pipe(map(value => value * 10));
 
 const addItem = (value: string) => {
   const parent = document.querySelector('ul');
@@ -10,13 +10,4 @@ const addItem = (value: string) => {
   parent.appendChild(ele);
 }
 
-const subscription = subject.subscribe(addItem);
-
-subject.next('I am a Subject');
-subject.next('I am also a Observable');
-subject.next('And I am hot');
-
-const subscription2 = subject.subscribe(value => addItem(`Subscriber2: ${value}`));
-
-subject.complete();
-subject.next('Heelo');
+const subscription = observable.subscribe(addItem);
