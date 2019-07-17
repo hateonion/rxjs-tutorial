1f856feead5bd3d0f40c1c6480ff7be23f5ae503
hateonion <ouyang_yingjie@outlook.com>
2019-07-17 20:56:29 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import { Observable } from 'rxjs'; 
 
 const observable = Observable.create(observer => {
+  observer.next('Start');
+  observer.next('Yeah');
   setInterval(()=> {
     observer.next('I am alive')
   }, 1000)
 })
-
 
 const addItem = (value: string) => {
   const parent = document.querySelector('ul');
@@ -15,12 +16,4 @@ const addItem = (value: string) => {
 }
 
 const subscription = observable.subscribe(addItem);
-const subscription2 =  observable.subscribe((value) => addItem(`Subscriber2 ${value}`));
-
-subscription.add(subscription2);
-
-setTimeout(() => subscription.unsubscribe(), 3000)
-
-
-
-
+setTimeout(()=> observable.subscribe((value) => addItem(`Subscriber2 ${value}`)), 2000);
