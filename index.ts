7ebf9fdc2cbee0d720323a887b4366714af4e356
hateonion <ouyang_yingjie@outlook.com>
2019-12-11 23:12:38 +1100

    1 file modified

--- index.ts
+++ index.ts
@@ -2,7 +2,12 @@ import { Observable, of } from 'rxjs';
 import { share, switchMap, } from 'rxjs/operators';
 import {fromFetch} from 'rxjs/fetch';
 
-const coldObservable = of('https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty').pipe(switchMap(url => fromFetch(url)), share());
+// const hotObservable = of('https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty').pipe(switchMap(url => fromFetch(url)), share());
 
-const sub1 = coldObservable.subscribe(console.log);
-const sub2 = coldObservable.subscribe(console.log);
+// const sub1 = hotObservable.subscribe(console.log);
+// const sub2 = hotObservable.subscribe(console.log);
+
+const codeObservable = of('https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty').pipe(switchMap(url => fromFetch(url)));
+
+const sub1 = codeObservable.subscribe(console.log);
+const sub2 = codeObservable.subscribe(console.log);
